01bc1e0c547d4cc7bcd5f779493cef456c04c65b
Alex <alexandriaduell@gmail.com>
2023-01-24 17:51:10 -0500

    Started authorization

--- api/auth/secrets.js
+++ api/auth/secrets.js
@@ -1,4 +1,4 @@
-// module.exports = {
-//     BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 12,
-//     JWT_SECRET: process.env.JWT_SECRET || `youGotThis!`
-// }
+module.exports = {
+    BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 12,
+    JWT_SECRET: process.env.JWT_SECRET || `youGotThis!`
+}
