40ff25089d89936e1a3c276722d459723d912c10
Alex <alexandriaduell@gmail.com>
2023-01-24 18:07:49 -0500

    auth-router complete

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -28,5 +28,37 @@ router.post(`/register`, (req, res) => {
 })
 
 router.post(`/login`, (req, res) => {
+    let { username, password } = req.body
 
+    Users.findBy({ username })
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)){
+                const token = buildToken(user)
+
+                res.status(200).json({
+                    message: `Welcome back ${user.username}`,
+                    token
+                })
+            }
+            else{
+                res.status(401).json({ 
+                    message: `Invalid Credentials`
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: `Error occurred in auth-router /login: ${err}`
+            })
+        })
 })
+
+const buildToken = user => {
+    const payload = {
+        subject: user.user_id,
+        username: user.username,
+        expiresIn: `1d`
+    }
+
+    return jwt.sign(payload, JWT_SECRET)
+}
